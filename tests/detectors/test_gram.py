4526bca7817d7601eb2165d817b8665c78181b66
TLanger <t.langer93@outlook.de>
2025-01-15 12:58:18 +0000

    remove duplicated code

--- tests/detectors/test_gram.py
+++ tests/detectors/test_gram.py
@@ -5,20 +5,11 @@ from torch.utils.data import DataLoader, TensorDataset
 
 from src.pytorch_ood.api import RequiresFittingException
 from src.pytorch_ood.detector import Gram
-from tests.helpers import ClassificationModel
 from tests.helpers.model import ConvClassifier
-import numpy as np
 
 
-class GramTest(unittest.TestCase):
-    """
-    Test gram matrix based method
-    """
-
-    def setUp(self) -> None:
-        torch.manual_seed(123)
-
-    def test_something(self):
+class InitGram:
+    def __init__(self):
         nn = ConvClassifier(in_channels=3, num_outputs=2)
 
         class MyHead(torch.nn.Module):
@@ -34,10 +25,20 @@ class GramTest(unittest.TestCase):
                 x = self.classifier(x)
                 return x
 
-        model = Gram(MyHead(nn.classifier, nn.pool), [nn.layer1], 2, [1])
+        self.model = Gram(MyHead(nn.classifier, nn.pool), [nn.layer1], 2, [1])
 
-        # use integers as labels
-        # y= torch.cat([torch.zeros(size=(10,),dtype=torch.int), torch.ones(size=(10,))])
+
+class GramTest(unittest.TestCase):
+    """
+    Test gram matrix based method
+    """
+
+    def setUp(self) -> None:
+        torch.manual_seed(123)
+
+    def test_something(self):
+
+        model = InitGram().model
         y = torch.cat(
             [torch.zeros(size=(10,), dtype=torch.int), torch.ones(size=(10,), dtype=torch.int)]
         )
@@ -54,22 +55,7 @@ class GramTest(unittest.TestCase):
         self.assertEqual(scores.shape[0], 20)
 
     def test_nofit(self):
-        nn = ConvClassifier(in_channels=3, num_outputs=2)
-
-        class MyHead(torch.nn.Module):
-            def __init__(self, classifier, pool):
-                super(MyHead, self).__init__()
-                self.classifier = classifier
-                self.pool = pool
-                self.flatten = torch.nn.Flatten()
-
-            def forward(self, x):
-                x = self.pool(x)
-                x = self.flatten(x)
-                x = self.classifier(x)
-                return x
-
-        model = Gram(MyHead(nn.classifier, nn.pool), [nn.layer1], 2, [1])
+        model = InitGram().model
         x = torch.randn(size=(20, 10))
 
         with self.assertRaises(RequiresFittingException):
